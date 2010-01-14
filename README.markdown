56ba42a868e81e06da93ee1fb03e43145d474798
Artur Ventura <artur.ventura@gmail.com>
2010-01-14 16:45:42 +0000

    first commit

---
+++ README.markdown
@@ -0,0 +1,7 @@
+CircularPhoto
+=============
+
+This is the code for CircularPhoto. This was a simple photographic
+atempt to create a recursive photo.
+
+You can try it at http://recursivephoto.appspot.com/ .
---
+++ js/layout.js
@@ -0,0 +1,20 @@
+(function($){
+	var initLayout = function() {
+		$('#myGallery').spacegallery({loadingClass: 'loading'});
+	};
+	
+	var showTab = function(e) {
+		var tabIndex = $('ul.navigationTabs a')
+							.removeClass('active')
+							.index(this);
+		$(this)
+			.addClass('active')
+			.blur();
+		$('div.tab')
+			.hide()
+				.eq(tabIndex)
+				.show();
+	};
+	
+	EYE.register(initLayout, 'init');
+})(jQuery)
